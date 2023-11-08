50ccf41e45320dd0c88759b10eab569638e775c1
hgirish <girish@post.com>
2023-11-07 22:35:52 -0800

    jinja template

--- Learning/webapp/app.py
+++ Learning/webapp/app.py
@@ -1,29 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, session
 import os
 import swimclub
 
 
 app = Flask(__name__)
+app.secret_key = "You will never guess..."
 
 
 @app.get("/")
 def index():
-    return "This is a placeholder for your webapp's opening page."
+    return render_template("index.html")
 
 
 @app.get("/swimmers")
 def display_swimmers():
+    populate_data()
+    return str(sorted(session["swimmers"]))
+
+@app.get("/files/<swimmer>")
+def get_swimmers_files(swimmer):
+    populate_data()
+    return str(session["swimmers"][swimmer])
+
+def populate_data():
+    if "swimmers" in session:
+        return
     swim_files = os.listdir(swimclub.FOLDER)
     if ".DS_Store" in swim_files:
         swim_files.remove(".DS_Store")
-    swimmers = {}
+    session["swimmers"] = {}
     for file in swim_files:
         name, *_ = swimclub.read_swim_data(file)
-        if name not in swimmers:
-            swimmers[name] = []
-        swimmers[name].append(file)
-    return str(sorted(swimmers))
+        if name not in session["swimmers"]:
+            session["swimmers"][name] = []
+        session["swimmers"][name].append(file)
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
